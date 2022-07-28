e56d9d6d42400238c9f0cabaa6b9e4684b943fb3
Brett Ezell <dnbeze55@gmail.com>
2022-07-27 23:06:06 -0400

    this is a mess im only pushing to share progress NOT working

--- fitness/models.py
+++ fitness/models.py
@@ -1,3 +1,24 @@
 from django.db import models
-
+from django.db.models import Q # for queries
 # Create your models here.
+
+class Exercises(models.Model):
+    name = models.CharField(max_length=60)
+    description = models.TextField(max_length=200)
+    image = models.ImageField(upload_to='images/')
+    musclegroup = models.PositiveSmallIntegerField(
+        choices=(
+            (1,"Hamstrings"),
+            (2,"Calves"),
+            (3,"Chest"),
+            (4,"Back"), 
+            (5,"Shoulders"),
+            (6,"Triceps"),
+            (7,"Biceps"),
+            (8,"Forearms"),
+            (9,"Traps"),
+            (10,"Abs")
+        ))
+    text = models.TextField()
+    def __str__(self):
+        return self.name
--- fitness/views.py
+++ fitness/views.py
@@ -1,6 +1,25 @@
+from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
 
 # Create your views here.
 
 def index(request):
     return render(request, 'fitness/index.html')
+
+def signin(request):
+    return render(request, 'fitness/signin.html')
+
+def signup(request):
+    return render(request, 'fitness/signup.html')
+
+def viewworkouts(request):
+    return render(request, 'fitness/viewworkouts.html')
+    
+
+def selectworkouts(request, selectedworkout):
+    try:
+        result = workouts[selectedworkout]
+        return HttpResponse(result)
+    except KeyError:
+        return HttpResponseNotFound("Workout not found!!!")
+    
